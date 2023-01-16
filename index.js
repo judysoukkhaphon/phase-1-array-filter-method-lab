90f5878ab534ca170466b176b7e05c597e74bab2
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-16 09:36:12 -0500

    accessing arrays through loops

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    let names=[];
+    let decasedString = string.toLowerCase();
+    for (let i = 0; i < drivers.length; i++){
+        let decasedName = drivers[i].toLowerCase();
+        if (decasedName == decasedString){
+            names.push(drivers[i]);
+        }
+    }
+    return names;
+}
+
+function fuzzyMatch(drivers, string) {
+    const names = [];
+    const strlen = string.length;
+    let match = false;
+    for (let i = 0; i < drivers.length; i++){
+        for (let s = 0; s < strlen; s++){
+            if (drivers[i].charAt(s) === string.charAt(s)){
+                match = true;
+            }
+            else{
+                match = false;
+            }
+        }
+        if (match == true){
+            names.push(drivers[i]);
+        }
+    }
+    return names;
+}
+
+function matchName(driverInfo, string) {
+    const driversList = [];
+    for(let i = 0; i < driverInfo.length; i++){
+           let driverName = driverInfo[i].name;
+            if (driverName === string){
+                driversList.push(driverInfo[i]);
+            }
+        
+    }
+    return driversList;
+
+}
